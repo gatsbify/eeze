1f0611786cb94c56c5793c0ae38f5cb81ea9c974
botmasterza <flo@gatsbify.site>
2020-05-11 23:02:10 +0200

    23a

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,19 +44,19 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiUrl: process.env.DEPLOY_URL
-          ? "https://eezesubmit.netlify.app/"
-          : `http://localhost:1337`,
-        queryLimit: 1000, // Default to 100
-        contentTypes: ["company", "user"],
-        //     // Possibility to login with a strapi user, when content types are not publically available (optional).
-        //  loginData: {
-        //    identifier: "",
-        //    password: "",
-        //  },
-      },
+      //   resolve: `gatsby-source-strapi`,
+      //   options: {
+      //     apiUrl: process.env.DEPLOY_URL
+      //       ? "https://eezesubmit.netlify.app/"
+      //       : `http://localhost:1337`,
+      //     queryLimit: 1000, // Default to 100
+      //     contentTypes: ["company", "user"],
+      //     // Possibility to login with a strapi user, when content types are not publically available (optional).
+      //  loginData: {
+      //    identifier: "",
+      //    password: "",
+      //  },
+      // },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
